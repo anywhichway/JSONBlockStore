0ad796d8c4077c2095eeae204d4770e8a660928a
Simon Blackwell <syblackwell@anywhichway.com>
2017-06-29 20:28:45 -0400

    updated to v0.0.2 of BlockStore

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
 # JSONBlockStore
 Single file, block allocated storage for JSON objects.
 
-Objects are retrievable by key or line number.
-
-Reads over 100,000 objects second for small size records, i.e. 100 bytes.
-
-Writes over 25,000 objects second for small size records, i.e. 100 bytes.
+A thin wrapper around BlockStore.
--- test/load.js
+++ test/load.js
@@ -9,7 +9,7 @@ async function test() {
 	await bstore.set("1",{name:"Joe",age:28});
 	one = await bstore.get("1");
 	console.log(one);
-	const testsize = 20000;
+	const testsize = 1000;
 	for(let i=0;i<testsize;i++) {
 		await bstore.set("1",{name:"Joe",age:27});
 	}
@@ -25,18 +25,14 @@ async function test() {
 	}
 	let end = Date.now();
 	console.log("Write Records Sec:", testsize / ((end-start)/1000));
-	console.log("Adds OK");
-	let content = await bstore.load(0,20);
-	console.log(content);
-	content = await bstore.load(1);
-	console.log(content);
 	start = Date.now();
-	content = await bstore.load(0,testsize);
+	for(let i=0;i<testsize;i++) {
+		await bstore.get(i);
+	}
 	end = Date.now();
 	console.log("Read Records Sec:", testsize / ((end-start)/1000));
-	console.log(content);
+	console.log(await bstore.get(testsize/2));
 }
-
 test();
 
 
